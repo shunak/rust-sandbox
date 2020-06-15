4d638b0e1f63f8ad0412c779733956d7f63486de
shunak <shuna966@gmail.com>
2020-06-15 15:31:08 +0900

    privacy rule

---
+++ privacy/target/debug/deps/privacy-f6d789c87061b0af.d
@@ -0,0 +1,7 @@
+/Users/syu/projects/privacy/target/debug/deps/privacy-f6d789c87061b0af.rmeta: src/lib.rs
+
+/Users/syu/projects/privacy/target/debug/deps/libprivacy-f6d789c87061b0af.rlib: src/lib.rs
+
+/Users/syu/projects/privacy/target/debug/deps/privacy-f6d789c87061b0af.d: src/lib.rs
+
+src/lib.rs:
