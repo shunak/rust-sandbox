ff4ca15b209933de15cb47bbee0f27d478b51e42
shunak <shuna966@gmail.com>
2020-06-26 12:46:08 +0900

    default implementation

---
+++ trait_excr/target/debug/deps/trait_excr-e63e906ab6880144.d
@@ -0,0 +1,5 @@
+/Users/syu/projects/trait_excr/target/debug/deps/trait_excr-e63e906ab6880144: src/main.rs
+
+/Users/syu/projects/trait_excr/target/debug/deps/trait_excr-e63e906ab6880144.d: src/main.rs
+
+src/main.rs:
