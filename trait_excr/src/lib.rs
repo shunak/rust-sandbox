f53aec4e693e05d9263d85e96d1734379d2e9f91
shunak <shuna966@gmail.com>
2020-06-26 11:31:01 +0900

    implementation Summary trait on NewsArticle Type and Tweet Type

--- trait_excr/src/lib.rs
+++ trait_excr/src/lib.rs
@@ -1,4 +1,26 @@
-pub trait Summary{
-	fn summerize(&self) -> String;
+pub struct NewsArticle{
+	pub headline: String,
+	pub lovation: String,
+	pub author: String,
+	pub content: String,
+}
+
+impl Summary for NewsArticle {
+	fn summerize(&self)->String{
+		format!("{}, by {} ({})", self.headline, self.author, self.location)
+	}
+}
+
+pub struct Tweet{
+	pub username: String,
+	pub content: String,
+	pub reply: bool,
+	pub retweet: bool,
+}
+
+impl Summary for Tweet {
+	fn summerize(&self)->String{
+		format!("{}: {}", self.username, self.content)
+	}
 }
 
