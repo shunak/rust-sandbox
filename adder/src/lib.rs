23c4168e0f3ee64bee93176b56c61ca2e64e4a73
shunak <shuna966@gmail.com>
2020-07-08 18:43:30 +0900

    assert_eq & assert_ne

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,34 +1,14 @@
-#[derive(Debug)]
-pub struct Rectangle {
-	length: u32,
-	width: u32,
+pub fn add_two(a: i32) -> i32 {
+	a + 3 
 }
-
-impl Rectangle{
-	pub fn can_hold(&self, other: &Rectangle) -> bool{
-		self.length < other.length && self.width < other.width
-	}
-}
-
 
 #[cfg(test)]
-mod tests{
+mod tests {
 	use super::*;
+	
 	#[test]
-    	fn larger_can_hold_smaller() {
-        	let larger = Rectangle { length: 8, width: 7 };
-        	let smaller = Rectangle { length: 5, width: 1 };
-
-        	assert!(larger.can_hold(&smaller));
-    	}	
-	#[test]
-	fn smaller_cannot_hold_larger(){
-		let larger = Rectangle { length: 8, width: 7};
-		let smaller = Rectangle { length: 5, width: 1};
-		
-		assert!(smaller.can_hold(&larger));
+	fn it_adds_two(){
+		assert_eq!(4,add_two(2));
 	}
+
 }
-
-
-
