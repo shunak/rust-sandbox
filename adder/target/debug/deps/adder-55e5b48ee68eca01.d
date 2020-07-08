0c2fb6fd8428512d2361f12bd83ab20aa9da34da
shunak <shuna966@gmail.com>
2020-07-08 15:41:04 +0900

    test

---
+++ adder/target/debug/deps/adder-55e5b48ee68eca01.d
@@ -0,0 +1,7 @@
+/Users/syu/projects/adder/target/debug/deps/adder-55e5b48ee68eca01.rmeta: src/lib.rs
+
+/Users/syu/projects/adder/target/debug/deps/libadder-55e5b48ee68eca01.rlib: src/lib.rs
+
+/Users/syu/projects/adder/target/debug/deps/adder-55e5b48ee68eca01.d: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-9bbe61747343442b.d
@@ -0,0 +1,5 @@
+/Users/syu/projects/adder/target/debug/deps/adder-9bbe61747343442b: src/lib.rs
+
+/Users/syu/projects/adder/target/debug/deps/adder-9bbe61747343442b.d: src/lib.rs
+
+src/lib.rs:
