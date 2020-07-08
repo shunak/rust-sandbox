88f4b257206ba20b03719d4c920cb0124838bb4e
shunak <shuna966@gmail.com>
2020-07-08 18:54:21 +0900

    test message

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add_two(a: i32) -> i32 {
-	a + 3 
+pub fn greeting(name: &str) -> String{
+	String::from("Hello!")
 }
 
 #[cfg(test)]
-mod tests {
+mod tests{
 	use super::*;
 	
 	#[test]
-	fn it_adds_two(){
-		assert_eq!(4,add_two(2));
+	fn greeting_contains_name(){
+		let result = greeting("Carol");
+		assert!(
+			result.contains("Carol"),
+			"Greeting did not contain name, value was `{}`",result
+		);
 	}
 
 }
