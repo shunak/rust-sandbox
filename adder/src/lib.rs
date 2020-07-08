97dc37d1fa3a9a852e489d9f366fae6ebb6fb011
shunak <shuna966@gmail.com>
2020-07-08 18:34:56 +0900

    test 2

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -6,7 +6,7 @@ pub struct Rectangle {
 
 impl Rectangle{
 	pub fn can_hold(&self, other: &Rectangle) -> bool{
-		self.length > other.length && self.width > other.width
+		self.length < other.length && self.width < other.width
 	}
 }
 
@@ -14,13 +14,19 @@ impl Rectangle{
 #[cfg(test)]
 mod tests{
 	use super::*;
-	
 	#[test]
-	fn larger_can_hold_smaller(){
+    	fn larger_can_hold_smaller() {
+        	let larger = Rectangle { length: 8, width: 7 };
+        	let smaller = Rectangle { length: 5, width: 1 };
+
+        	assert!(larger.can_hold(&smaller));
+    	}	
+	#[test]
+	fn smaller_cannot_hold_larger(){
 		let larger = Rectangle { length: 8, width: 7};
 		let smaller = Rectangle { length: 5, width: 1};
 		
-		assert!(larger.can_hold(&smaller));
+		assert!(smaller.can_hold(&larger));
 	}
 }
 
