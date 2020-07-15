08e1239f2f5b2389bec9ea6301ce95f5695c7687
shunak <shuna966@gmail.com>
2020-07-15 17:51:19 +0900

    added argument val attention

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 
 fn main() {
-	let args: Vec<String>=env::args().collect();
+	let args: Vec<String>=env::args().collect();//assign argument value by String Type Vector.
 	
 	let query = &args[1];
 	let filename = &args[2];
