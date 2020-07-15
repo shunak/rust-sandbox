33f7967f343757e642d8c1e96311e7215d9f2af7
shunak <shuna966@gmail.com>
2020-07-15 18:26:04 +0900

    added: file reading conpoment

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
 	let args: Vec<String>=env::args().collect();//assign argument value by String Type Vector.
@@ -8,5 +10,15 @@ fn main() {
 	
 	println!("Searching for {}", query);
 	println!("In file {}", filename);
+
+	//file not found
+	let mut f = File::open(filename).expect("file not found");//filr hundler
+	let mut contents = String::new();// string type file contents holder
+	f.read_to_string(&mut contents)
+	.expect("something went wrong reading the file");
+
+	println!("With text:\n{}",contents);
+
+
 
 }
