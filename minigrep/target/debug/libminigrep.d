84b3cd21258bc930a4b00eba0c66edf3213bc162
shunak <shuna966@gmail.com>
2020-07-29 18:27:17 +0900

    extract argument analyzer

---
+++ minigrep/target/debug/libminigrep.d
@@ -0,0 +1,1 @@
+/Users/syu/projects/minigrep/target/debug/libminigrep.rlib: /Users/syu/projects/minigrep/src/lib.rs
--- minigrep/target/debug/minigrep.d
+++ minigrep/target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/Users/syu/projects/minigrep/target/debug/minigrep: /Users/syu/projects/minigrep/src/main.rs
+/Users/syu/projects/minigrep/target/debug/minigrep: /Users/syu/projects/minigrep/src/lib.rs /Users/syu/projects/minigrep/src/main.rs
