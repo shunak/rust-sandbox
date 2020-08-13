42b91826a02534c2c703c9f247731db222c8a743
shunak <shuna966@gmail.com>
2020-08-13 16:31:56 +0900

    error handling & extract run function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,18 +1,28 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
 	let args: Vec<String>=env::args().collect();//assign argument value by String Type Vector.
 	
-	let (query, filename) = parse_config(&args);	
+	let config = Config::new(&args).unwrap_or_else(|err| {
+		
+		println!("Problem parsing arguments: {}",err);
+		process::exit(1);
 
+	});
 	
-	println!("Searching for {}", query);
-	println!("In file {}", filename);
+	println!("Searching for {}",config.query);
+	println!("In file {}", config.filename);
+
+	run(config);
+}
+
+fn run(config: Config){//argument contains config instance
 
 	//file not found
-	let mut f = File::open(filename).expect("file not found");//filr hundler
+	let mut f = File::open(config.filename).expect("file not found");//filr hundler
 	let mut contents = String::new();// string type file contents holder
 	f.read_to_string(&mut contents)
 	.expect("something went wrong reading the file");
@@ -20,20 +30,28 @@ fn main() {
 	println!("With text:\n{}",contents);
 
 
-
 }
+
+
 
 struct Config{
 	query: String,
 	filename: String,
 }
 
+impl Config {
 
-fn parse_config(args: &[String])-> Config {
-	let query =  &args[1];
-	let filename = &args[2];
+	fn new(args: &[String])-> Result<Config, &'static str> {
+		if args.len() < 3{
+			return Err("not enough arguments");
+		}	
+
+		let query =  args[1].clone();
+		let filename = args[2].clone();
 	
-	Confing { query, filename }
+		Ok(Config { query, filename })
+	}
+
 }
 
 
