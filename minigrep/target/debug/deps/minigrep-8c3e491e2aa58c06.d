a81cd15b8408f57daaa76e12852b72645425a6da
shunak <shuna966@gmail.com>
2020-09-03 18:59:10 +0900

    development library function by TDD

---
+++ minigrep/target/debug/deps/minigrep-8c3e491e2aa58c06.d
@@ -0,0 +1,5 @@
+/Users/syu/projects/minigrep/target/debug/deps/minigrep-8c3e491e2aa58c06: src/main.rs
+
+/Users/syu/projects/minigrep/target/debug/deps/minigrep-8c3e491e2aa58c06.d: src/main.rs
+
+src/main.rs:
