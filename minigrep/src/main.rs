e9c655aa2490243ef139ae812368273b3d1ac21f
shunak <shuna966@gmail.com>
2020-09-06 00:02:10 +0900

    modify: err outputs by eprintln! 'former println!'

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,21 +6,16 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-	let args: Vec<String>=env::args().collect();//assign argument value by String Type Vector.
-	
-	let config = Config::new(&args).unwrap_or_else(|err| {
-		
-		println!("Problem parsing arguments: {}",err);
-		process::exit(1);
+    let args: Vec<String> = env::args().collect(); //assign argument value by String Type Vector.
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    // println!("Searching for {}",config.query);
+    // println!("In file {}", config.filename);
 
-	});
-	
-	println!("Searching for {}",config.query);
-	println!("In file {}", config.filename);
-
-	if let Err(e) = minigrep::run(config){
-		println!("Application error: {}",e);
-		
-		process::exit(1);
-	}
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
