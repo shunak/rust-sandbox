e7b1a722ef678dba4231b88aa0abff2947c93575
shunak <shuna966@gmail.com>
2020-09-11 18:00:10 +0900

    closure

---
+++ closure/target/debug/closure.d
@@ -0,0 +1,1 @@
+/Users/syu/projects/closure/target/debug/closure: /Users/syu/projects/closure/src/main.rs
---
+++ closure/target/debug/deps/closure-102c9da6645cccb7.d
@@ -0,0 +1,5 @@
+/Users/syu/projects/closure/target/debug/deps/closure-102c9da6645cccb7: src/main.rs
+
+/Users/syu/projects/closure/target/debug/deps/closure-102c9da6645cccb7.d: src/main.rs
+
+src/main.rs:
